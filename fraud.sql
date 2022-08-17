0ff2d0c6281c1514cd1d9293b826f44ff115498b
unknown <johnathan.overton83@gmail.com>
2022-08-17 16:06:10 -0600

    Grouping Transactions by Cardholder

---
+++ fraud.sql
@@ -0,0 +1,74 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Eoypbj
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- create tables for merchant and card holder data, add Constraints as well as Foreign and Primary Keys
+
+
+CREATE TABLE "card_holder" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(60)   NOT NULL,
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "credit_card" (
+    "card" VARCHAR(20)   NOT NULL,
+    "cardholder_id" INT   NOT NULL,
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
+        "card"
+     )
+);
+
+CREATE TABLE "merchant" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(60)   NOT NULL,
+    "id_merchant_category" INT   NOT NULL,
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant_category" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_merchant_category" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "transaction" (
+    "id" INT   NOT NULL,
+    "date" TIMESTAMP   NOT NULL,
+    "amount" FLOAT   NOT NULL,
+    "card" VARCHAR(20)   NOT NULL,
+    "id_merchant" INT   NOT NULL,
+    CONSTRAINT "pk_transaction" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_cardholder_id" FOREIGN KEY("cardholder_id")
+REFERENCES "card_holder" ("id");
+
+ALTER TABLE "merchant" ADD CONSTRAINT "fk_merchant_id_merchant_category" FOREIGN KEY("id_merchant_category")
+REFERENCES "merchant_category" ("id");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_card" FOREIGN KEY("card")
+REFERENCES "credit_card" ("card");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "fk_transaction_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
+
+-- queries to identify possible fraud
+
+-- group transactions by cardholder
+
+SELECT card_holder.name, credit_card.card, transaction.date, transaction.amount, merchant.name
+   , merchant_category.name
+FROM transaction
+JOIN credit_card ON credit_card.card = transaction.card
+JOIN card_holder ON card_holder.id = credit_card.cardholder_id
+JOIN merchant ON merchant.id = transaction.id_merchant
+JOIN merchant_category ON merchant_category.id = merchant.id_merchant_category
